132836f419018c265d3f3a375bef73ffb21e94a5
musiur <musiur.opu@gmail.com>
2023-07-16 17:56:40 +0600

    comp-dev:: room details: added with completion message

--- src/components/pages/register/room-details/room-details.js
+++ src/components/pages/register/room-details/room-details.js
@@ -1,5 +1,18 @@
+import IconWrapper from "@/components/core/icon-wrapper/icon-wrapper";
+import { Check } from "lucide-react";
+
 const RoomDetails = () => {
-  return <div>Room details</div>;
+  return (
+    <div className="section-d max-w-[500px]">
+      <p className="px-3 py-2 rounded-md bg-green-400 text-white flex items-center justify-center gap-2">
+        <IconWrapper>
+          <Check />
+        </IconWrapper>
+        <span>Your property details information all are collected!</span>
+      </p>
+      <h3 className="font-bold my-5">Room details</h3>
+    </div>
+  );
 };
 
 export default RoomDetails;
