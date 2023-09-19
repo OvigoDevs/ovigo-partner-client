e3acffde0a2f809cafcb74f6eed25ef3f0a8315d
noyon-podder <noyon.vai369@gmail.com>
2023-09-19 22:47:43 +0600

    hero section add this commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-
+import Hero from "@/components/ui/hero";
 
 const Home = () => {
   return (
-    <div className="container section-p min-h-screen">
-      <h1 className="pb-10 text-xl font-black">Home</h1>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 };
 
--- src/components/ui/mode-toggle.jsx
+++ src/components/ui/mode-toggle.jsx
@@ -18,11 +18,11 @@ export function ModeToggle() {
   return (
     <DropdownMenu className=" w-[40px] h-[40px] ">
       <DropdownMenuTrigger asChild>
-        <Button className="">
+        <div className="w-10 h-10 rounded-full bg-[#cff1ef] text-[#136f40] duration-300 flex items-center justify-center cursor-pointer">
           <Sun className="h-[1.5  rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
-        </Button>
+        </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
